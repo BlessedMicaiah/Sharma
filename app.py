78ff65988deaef91ce862f827704d575e697f43f
Faithology <43255094+BlessedMicaiah@users.noreply.github.com>
2025-03-28 23:17:44 -0500

    Prepare application for Render deployment

--- app.py
+++ app.py
@@ -307,4 +307,5 @@ Answer:"""
     return bot_response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
